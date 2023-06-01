5143750a4fc5647ac6355e232250e28296653b3d
Neel Akbari <neel.akbari@artoon.in>
2023-06-01 18:23:25 +0530

    initail todo list commit

---
+++ TodoList/dist/js/main.js
@@ -0,0 +1,13 @@
+// function fetchRandomImage() {
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//         .then(response => response.json())
+//         .then(data => {
+//             const imageUrl = data.message; // Assuming the API response has a 'url' property containing the image URL
+//             const imgElement = document.getElementById('randomImage');
+//             imgElement.src = imageUrl;
+//             console.log(data)
+//         })
+//         .catch(error => {
+//             console.error('Error:', error);
+//         });
+// }
